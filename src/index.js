2ed39171f8ccd078fb0fa7ac46da30db0a219133
Sargurunathan <620943@cognizant.com>
2020-02-06 14:42:15 +0530

    first commit

---
+++ src/index.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+// import Login from './Login'
+import Register from './Register'
+
+ReactDOM.render(
+  <Register/>,
+  document.getElementById('app')
+);
